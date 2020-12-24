a14b452e50c9af54675095203148eeab8ef3889d
BuildTools <firatyusuf.d@gmail.com>
2020-12-24 01:29:31 +0100

    COCK
    SUCK MY COCK ILMAR

--- commands/stfu.js
+++ commands/stfu.js
@@ -1,24 +1,38 @@
 const fs = require("fs");
-let rawcock = fs.readFileSync("cock.json");
+let rawcock = fs.readFileSync("./cock.json");
 let cock = JSON.parse(rawcock);
 module.exports = {
-	execute(message) {
-		if (message.content.startsWith('!addstfu')) {
-			const words = message.content.split(' ');
-			let stfuid = words[1];
-			if (stfuid == undefined||stfuid.length !== 18||isNaN(stfuid)||isNaN(stfuid)) {
-				const user = message.author;
-				message.channel.send(`FUCK YOU ${user.username} :angry: :angry: `);
-				return;
-			}
-			rawcock = fs.readFileSync("cock.json");
-			cock = JSON.parse(rawcock);
-			cock[cock].push(stfuid)
-			let cockdata = JSON.stringify(cock);
-			fs.writeFileSync("cock.json", cockdata);
-		}
-		if (message.author.id == stfuid) {
-			message.channel.send('stfu');
-		}
-	},
+  execute(message) {
+    if (message.content.startsWith("!addstfu")) {
+      const words = message.content.split(" ");
+      let stfuid = words[1];
+      if (
+        stfuid == undefined ||
+        stfuid.length !== 18 ||
+        isNaN(stfuid) ||
+        isNaN(stfuid)
+      ) {
+        const user = message.author;
+        message.channel.send(`FUCK YOU ${user.username} :angry: :angry: `);
+        return;
+      }
+      rawcock = fs.readFileSync("cock.json");
+      console.log(rawcock);
+      cock = JSON.parse(rawcock);
+      console.log(cock);
+      if (!cock.cocks.includes(stfuid)) {
+        cock.cocks.push(stfuid);
+        let cockdata = JSON.stringify(cock);
+        fs.writeFileSync("cock.json", cockdata);
+      } else {
+        message.reply("FUCK YOU NIGGA");
+      }
+      return;
+    }
+    rawcock = fs.readFileSync("./cock.json");
+    cock = JSON.parse(rawcock);
+    if (cock.cocks.includes(message.author.id)) {
+      message.channel.send("stfu");
+    }
+  },
 };
