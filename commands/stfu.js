ed0f290aadb11e9c9488ae6b1db455fee6cc720d
ilmar <ramli@gmail.com>
2020-12-25 01:32:55 +0200

    Added documentation to everything

--- commands/stfu.js
+++ commands/stfu.js
@@ -1,36 +1,48 @@
-const fs = require("fs");
-let rawdata = fs.readFileSync("./ids.json");
-let ids = JSON.parse(rawdata);
+const fs = require('fs');
+//	The fs module enables interacting with the file system in a way modeled on standard POSIX functions.
+const path = require('path');
+//	The path module provides utilities for working with file and directory paths
+let rawdata = fs.readFileSync(path.resolve('commands', 'jsonFiles', 'ids.json'));
+// Find the correct ids.json file
+let rawdataparsed = JSON.parse(rawdata);
+//	Converts it to ASCII text, and parses the actual JSON data in to a JavaScript object.
 module.exports = {
-  execute(message) {
-    if (message.content.startsWith("!addstfu")) {
-      const words = message.content.split(" ");
-      let stfuid = words[1];
-      if (
-        stfuid == undefined ||
+	execute(message) {
+		if (message.content.startsWith('!addstfu')) {
+			const words = message.content.split(' ');
+			const stfuid = words[1];
+			// If message starts with !addstfu, split the message and make stfuid equal to the string of numbers after !addstfu.
+			if (
+				stfuid == undefined ||
         stfuid.length !== 18 ||
-        isNaN(stfuid) ||
         isNaN(stfuid)
-      ) {
-        const user = message.author;
-        message.channel.send(`FUCK YOU ${user.username} :angry: :angry: `);
-        return;
-      }
-      rawdata = fs.readFileSync("ids.json");
-      ids = JSON.parse(rawdata);
-      if (!ids.ids.includes(stfuid)) {
-        ids.ids.push(stfuid);
-        let idsdata = JSON.stringify(ids);
-        fs.writeFileSync("ids.json", idsdata);
-      } else {
-        message.reply("Id already is in the list");
-      }
-      return;
-    }
-    rawdata = fs.readFileSync("./ids.json");
-    ids = JSON.parse(rawdata);
-    if (ids.ids.includes(message.author.id)) {
-      message.channel.send("stfu");
-    }
-  },
+			) {
+				message.reply('Not a valid discord id! :angry: :angry: ');
+				return;
+				//	If stfuid, is undefined, not 18 chars, or not a number, send an error message back to the user.
+			}
+			rawdata = fs.readFileSync(path.resolve('commands', 'jsonFiles', 'ids.json'));
+			rawdataparsed = JSON.parse(rawdata);
+			if (!rawdataparsed.ids.includes(stfuid)) {
+				//	If ids.json doesnt include the discord id
+				rawdataparsed.ids.push(stfuid);
+				//	Pushes new id to the end of the ids array in ids.json
+				const idsdata = JSON.stringify(rawdataparsed);
+				//	Stringify method converts a JavaScript object or value to a JSON string
+				const writepath = path.resolve('commands', 'jsonFiles', 'ids.json');
+				fs.writeFileSync(writepath, idsdata);
+				// Writes new id into ids.json asynchronously
+			}
+			else {
+				message.reply('Id already is in the list');
+			}
+			return;
+		}
+		rawdata = fs.readFileSync(path.resolve('commands', 'jsonFiles', 'ids.json'));
+		rawdataparsed = JSON.parse(rawdata);
+		if (rawdataparsed.ids.includes(message.author.id)) {
+			message.channel.send('stfu');
+			//	If discord id is in ids.json, sends stfu to the same text channel
+		}
+	},
 };
--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const fs = require('fs');
+//	The fs module enables interacting with the file system in a way modeled on standard POSIX functions.
 const Discord = require('discord.js');
+//	Imports node module discord.js
 const {
 	token,
 } = require('./config.json');
-
+//	Checks config.json for token
 const client = new Discord.Client();
+// Create a new Discord client
 
 const commandnames = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+//	Looks for commands folder and filters all files that end with .js
 
 for (const file of commandnames) {
 	const command = require(`./commands/${file}`);
+	//	Loops through all the .js files in commands, to see which can be executed
 
 	client.once('ready', () => {
 		console.log('Ready!');
 	});
+	// When the client is ready, run this code
+	// This event will only trigger one time after logging in
 
 	client.on('message', message => {
+		//	Whenever a message is sent inside a channel this bot has access to, the message is checked by the bot for possible commands etc
 		if (message.author.bot) return;
+		// If the message is sent by a bot, ignore it
 
 		command.execute(message);
-
+		//	Runs the function in all of the command .js files
 	});
 
 }
 client.login(token);
+// login to Discord with your app's token
